adcf91ceb667ddcb822986ce10931d82a8843fc3
gueberso <guigui1991@live.fr>
2025-09-24 17:35:56 +0200

    feat: temporary wordpress entrypoint script

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 set -e
 
+
+mkdir -p /var/www/html
+rm -rf /var/www/html/*
+
+cd /var/www/html
+
 wp core download
 
 # replace by looking into secrets files, right now not secured {cat secret/path/to/file/expected}
+# for now, --dbhost=3306:3306
+# tested, working if wordpress is on the same network as mariadb, otherwise its a mess
 wp config create \
 	--dbhost=mariadb:3306 \
 	--dbname=db \
 	--dbuser=mysql \
-	--dbpass=gueberso
+	--dbpass=gueberso \
+	--allow-root
 
+# tested, good
 wp core install \
-	--url=$DOMAIN \
-	--title=Temp \
-	--admin_user=mysql \
-	--admin_email=temp@email.mail --skip-email \
-	--locale=en_US # this one is by default, might want to delete it since its not relevant
+	--url="https://$DOMAIN" \
+	--title="Temp" \
+	--admin_user="mysql" \
+	--admin_password="gueberso" \
+	--admin_email="temp@email.mail" \
+	--skip-email \
+	--allow-root
+
+# completly lost from here
 
 # Start php-fpm
-exec php-fpm83 --nodaemonize --fpm-config /etc/php83/www.conf
+
+exec $@
