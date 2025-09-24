8a4baeef9fb2806ceb9cae49dc55f950792b7758
gueberso <guigui1991@live.fr>
2025-09-24 11:11:09 +0200

    feat: added a new wordpress-dev service to configure wordpress inside a container

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -3,13 +3,19 @@ set -e
 
 wp core download
 
-# replace by looking into secrest file, rn not secured {cat secret/path/to/file/expected}
-wp config create --dbhost=mariadb:3306 --dbname=db --dbuser=mysql --dbpass=gueberso
+# replace by looking into secrets files, right now not secured {cat secret/path/to/file/expected}
+wp config create \
+	--dbhost=mariadb:3306 \
+	--dbname=db \
+	--dbuser=mysql \
+	--dbpass=gueberso
 
-wp core install --url=$DOMAIN --title=Temp --admin_user=mysql --admin_email=temp@email.mail --skip-email --locale=en_US
-
-
-
+wp core install \
+	--url=$DOMAIN \
+	--title=Temp \
+	--admin_user=mysql \
+	--admin_email=temp@email.mail --skip-email \
+	--locale=en_US # this one is by default, might want to delete it since its not relevant
 
 # Start php-fpm
 exec php-fpm83 --nodaemonize --fpm-config /etc/php83/www.conf
