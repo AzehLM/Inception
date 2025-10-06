2803564c1e1e66131261f8a2998e6cd84b543a46
gueberso <guigui1991@live.fr>
2025-10-06 10:09:48 +0200

    test: trying to fix grafana metrics

--- srcs/requirements/bonus/grafana/tools/entrypoint.sh
+++ srcs/requirements/bonus/grafana/tools/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 
-# Read secrets
 if [ -f /run/secrets/grafana_admin_user ]; then
     export GF_SECURITY_ADMIN_USER=$(cat /run/secrets/grafana_admin_user)
 fi
@@ -10,5 +9,4 @@ if [ -f /run/secrets/grafana_admin_password ]; then
     export GF_SECURITY_ADMIN_PASSWORD=$(cat /run/secrets/grafana_admin_password)
 fi
 
-# Switch to grafana user and execute command
 exec su-exec grafana "$@"
